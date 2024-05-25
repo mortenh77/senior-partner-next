99bc056db2bcf07fc4c5b559a4fbb40e37f3d7c2
Morten Helgaland <84070562+mortenh77@users.noreply.github.com>
2024-05-25 22:58:38 +0200

    Added tailwind

--- app/components/company-widget.tsx
+++ app/components/company-widget.tsx
@@ -15,8 +15,20 @@ const CompanyWidget = ({ companyData }) => {
     );
   }
 
-  const companyName = companyData._embedded.enheter[0].navn;
+  const rawcompanyName = companyData._embedded.enheter[0].navn;
+  const companyName = rawcompanyName.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase());
+
+  // Check if the last word is two characters only
+  const words = companyName.split(" ");
+  const lastWord = words[words.length - 1];
+  if (lastWord.length === 2 || lastWord.length === 3) {
+    words[words.length - 1] = lastWord.toUpperCase();
+  }
+
+  const formattedCompanyName = words.join(" ");
   const url = companyData._embedded.enheter[0].hjemmeside;
+
+  // Her må jeg sjekke hvilken valuta som er brukt i resultatregnskapet og så formatere på den måte.
   const aarsresultat = new Intl.NumberFormat("nb-NO", { style: "currency", currency: "NOK" }).format(companyData.resultatregnskapResultat.aarsresultat);
   const sumKortsiktigGjeld = new Intl.NumberFormat("nb-NO", { style: "currency", currency: "NOK" }).format(companyData.egenkapitalGjeld.gjeldOversikt.kortsiktigGjeld.sumKortsiktigGjeld);
   const sumLangsiktigGjeld = new Intl.NumberFormat("nb-NO", { style: "currency", currency: "NOK" }).format(companyData.egenkapitalGjeld.gjeldOversikt.langsiktigGjeld.sumLangsiktigGjeld);
@@ -28,9 +40,9 @@ const CompanyWidget = ({ companyData }) => {
         <p>{selskapstype}</p>
         {url ? <>
           <a href={url} target="_blank" rel="noreferrer">
-            <h2>{companyName}</h2>
+            <h2>{formattedCompanyName}</h2>
           </a>
-        </> : <h2>{companyName}</h2>}
+        </> : <h2>{formattedCompanyName}</h2>}
         <p>Årsresultat {aarsresultat}</p>
         <p>Kortsiktig gjeld {sumKortsiktigGjeld}</p>
         <p>Langsiktig gjeld {sumLangsiktigGjeld}</p>
